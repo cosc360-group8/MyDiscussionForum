56fe3b4325f2fa8bb116361a4c62a21f880b349d
minhhbt <buithienminh@gmail.com>
2021-04-12 21:25:16 -0400

    Added layout for User-manage and Post-manage.

--- admin-users.php
+++ admin-users.php
@@ -4,42 +4,40 @@
 
         <div class="left-sidebar">
             <ul>
-                <li><a href="#">Manage posts</a></li>
-                <li><a href="admin-users.php">Manage users</a></li>
+                <li><a href="admin.php">Manage posts</a></li>
+                <li><a href="#">Manage users</a></li>
                 <li><a href="">Manage topics</a></li>
             </ul>
         </div>
 
         <div class="admin-content">  
             <div class="button-group">
-                <a href="createPost.php">Add posts</a>
-                <a href="#">Manage posts</a>
+                <a href="#">Add users</a> <!-- missing html -->
+                <a href="#">Manage users</a>
             </div>
             <div class="content-body">
-                <h2 class="page-title">Manage posts</h2>
+                <h2 class="page-title">Manage users</h2>
                 <table>
                     <thead>
                         <th>ID</th>
-                        <th>Title</th>
-                        <th>Author</th>
+                        <th>Username</th>
+                        <th>Email</th>
                         <th colspan="3">Action</th>
                     </thead>
                     <tbody>
                         <tr>
                             <td>1</td>
-                            <td>Tenet Review</td>
-                            <td>Ed Chambers</td>
+                            <td>ed1782</td>
+                            <td>edchambers@hotmail.com</td>
                             <td><a class="edit" href="">Edit</a></td>
                             <td><a class="delete" href="">Delete</a></td>
-                            <!-- <td><a class="publish" href="">Publish</a></td> -->
                         </tr>
                         <tr>
                             <td>2</td>
-                            <td>Situation at Barcelona</td>
-                            <td>John Smith</td>
+                            <td>jsmith26</td>
+                            <td>jsmith26@gmail.com</td>
                             <td><a class="edit" href="">Edit</a></td>
                             <td><a class="delete" href="">Delete</a></td>
-                            <!-- <td><a class="publish" href="">Publish</a></td> -->
                         </tr>
                     </tbody>
                 </table>
